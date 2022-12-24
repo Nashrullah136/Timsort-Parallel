2abc98941bd4065667c11e43934d43d36616e12c
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-12-24 15:47:40 +0700

    openmp integer pointer

--- openmp.cpp
+++ openmp.cpp
@@ -7,16 +7,46 @@
 #include <helper.hpp>
 #include <string>
 
-std::vector<int> openmp(std::vector<int> data, int numberOfThread)
+// std::vector<int> openmp(std::vector<int> data, int numberOfThread)
+// {
+//     int dataPerThread = data.size() / numberOfThread;
+//     bool isFinish[numberOfThread] = {};
+//     auto start = data.begin();
+//     #pragma omp parallel num_threads(numberOfThread)
+//     {
+//         int id = omp_get_thread_num();
+//         TimSort<>::sort(start + id * dataPerThread, start + (id + 1) * dataPerThread);
+//         for (size_t i = 0; i < log2(4); i++)
+//         {
+//             if (id % int(pow(2, i + 1)) != 0)
+//             {
+//                 isFinish[id] = true;
+//                 break;
+//             }
+//             while (!isFinish[id + int(pow(2, i))]){}
+//             TimSort<>::merge(start + id * dataPerThread, start + (id + int(pow(2, i))) * dataPerThread, start + (id + int(pow(2, i + 1))) * dataPerThread);
+//         }
+//     }
+//     return data;
+// }
+
+int main(int argc, char **argv)
 {
-    int dataPerThread = data.size() / numberOfThread;
-    bool isFinish[numberOfThread] = {};
-    auto start = data.begin();
-    #pragma omp parallel num_threads(numberOfThread)
+    int size;
+    std::cin>>size;
+    int data[size];
+    int i = 0;
+    while(std::cin>>data[i++]){
+    }
+    int num_of_thread = omp_get_max_threads();
+    int dataPerThread = size / num_of_thread;
+    bool isFinish[num_of_thread] = {};
+    auto start = data;
+    #pragma omp parallel
     {
         int id = omp_get_thread_num();
         TimSort<>::sort(start + id * dataPerThread, start + (id + 1) * dataPerThread);
-        for (size_t i = 0; i < log2(4); i++)
+        for (size_t i = 0; i < log2(num_of_thread); i++)
         {
             if (id % int(pow(2, i + 1)) != 0)
             {
@@ -27,18 +57,14 @@ std::vector<int> openmp(std::vector<int> data, int numberOfThread)
             TimSort<>::merge(start + id * dataPerThread, start + (id + int(pow(2, i))) * dataPerThread, start + (id + int(pow(2, i + 1))) * dataPerThread);
         }
     }
-    return data;
-}
-
-int main(int argc, char **argv)
-{
-    std::vector<int> data;
-    int temp;
-    while(std::cin>>temp){
-        data.push_back(temp);
+    bool result = true;
+    for (size_t i = 0; i < size; i++)
+    {
+        if(i != data[i]){
+            result = false;
+            break;
+        }
     }
-    data = openmp(data, 4);
-    bool result = is_sorted(data);
     std::cout<<"OpenMP,"<<result<<std::endl;
     return 0;
 }
